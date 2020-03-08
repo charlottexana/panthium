96da93453a9db7679f101f1b066f2424838c8a66
Charlotte Waller <charlotteawaller@gmail.com>
2020-03-08 17:22:39 +0000

    Changing database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_08_082702) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "enquiries", force: :cascade do |t|
     t.date "date"
     t.string "name"
